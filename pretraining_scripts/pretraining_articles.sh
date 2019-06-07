e18525d7a1d0ba6c76a7c8a59f076e8f677f894f
maggieezzat <maggieezzat9@gmail.com>
2019-06-07 11:46:16 +0200

    finetuning articles

--- pretraining_scripts/pretraining_articles.sh
+++ pretraining_scripts/pretraining_articles.sh
@@ -4,7 +4,7 @@
 
 python3 /home/maggie/bert-german/run_pretraining.py \
 --input_file=/home/maggie/bert-german/tfrecord_lists/articles_512.csv \
---output_dir=gs://deep_speech_bucket/maggie/bert_chkpts/articles_512_6K_5e-5/ \
+--output_dir=gs://deep_speech_bucket/maggie/bert_chkpts/articles_512_15K_5e-5/ \
 --do_train=True \
 --do_eval=True \
 --do_lower=True \
@@ -12,7 +12,7 @@ python3 /home/maggie/bert-german/run_pretraining.py \
 --train_batch_size=128 \
 --max_seq_length=512 \
 --max_predictions_per_seq=77 \
---num_train_steps=6000 \
+--num_train_steps=15000 \
 --num_warmup_steps=1000 \
 --learning_rate=5e-5 \
 --use_tpu=True \
