dbefc303fc24c1fe507e70d5e30765c786c5aabe
maggieezzat <maggieezzat9@gmail.com>
2019-06-07 14:49:30 +0200

    finetuning articles

--- run_articles_classifier.sh
+++ run_articles_classifier.sh
@@ -53,12 +53,12 @@ python3 /home/maggie/bert-german/articles_classifier.py \
 --data_dir=gs://deep_speech_bucket/maggie/articles_data/ \
 --bert_config_file=/home/maggie/bert-german/config/bert_config_uncased.json \
 --vocab_file=/home/maggie/bert-german/vocab/vocab_bert_final_uncased_1000.txt \
---output_dir=gs://deep_speech_bucket/maggie/articles_results/articles_512_6K_5e-5/ \
+--output_dir=gs://deep_speech_bucket/maggie/articles_results/articles_512_15K_5e-5/ \
 --do_lower_case=True \
 --do_train=True \
 --do_eval=True \
 --do_predict=False \
 --use_tpu=True \
 --tpu_name=$TPU_NAME \
---init_checkpoint=gs://deep_speech_bucket/maggie/bert_chkpts/articles_512_6K_5e-5/
+--init_checkpoint=gs://deep_speech_bucket/maggie/bert_chkpts/articles_512_15K_5e-5/
 
