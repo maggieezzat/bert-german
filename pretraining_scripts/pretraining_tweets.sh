e3e13a939d44bc656239b29d15ea28ef0c5ebdc6
maggieezzat <maggieezzat9@gmail.com>
2019-06-12 13:59:31 +0200

    create tweets tfrecord

---
+++ pretraining_scripts/pretraining_tweets.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+
+python3 /home/maggie/bert-german/run_pretraining.py \
+--input_file=/home/maggie/bert-german/tfrecord_lists/tweets_512.csv \
+--output_dir=gs://deep_speech_bucket/maggie/bert_chkpts/tweets_512_10K_5e-5/ \
+--do_train=True \
+--do_eval=True \
+--do_lower=True \
+--bert_config_file=/home/maggie/bert-german/config/bert_config_uncased.json \
+--train_batch_size=128 \
+--max_seq_length=512 \
+--max_predictions_per_seq=77 \
+--num_train_steps=10000 \
+--num_warmup_steps=1000 \
+--learning_rate=5e-5 \
+--use_tpu=True \
+--tpu_name=$TPU_NAME \
+--tpu_zone=us-central1-f \
+--init_checkpoint=gs://deep_speech_bucket/maggie/bert_chkpts/bert/model.ckpt-90000
