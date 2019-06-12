966e38bd1d428f4caaac788403991896fff6bb70
maggieezzat <maggieezzat9@gmail.com>
2019-06-12 11:18:47 +0200

    finetuning tweets

--- run_emotions_classifier.sh
+++ run_emotions_classifier.sh
@@ -2,7 +2,7 @@
 
 
 python3 /home/maggie/bert-german/emotions_classifier.py \
---data_dir=gs://deep_speech_bucket/maggie/twitter-data/ \
+--data_dir=gs://deep_speech_bucket/maggie/twitter_data/ \
 --bert_config_file=/home/maggie/bert-german/config/bert_config_uncased.json \
 --vocab_file=/home/maggie/bert-german/vocab/vocab_bert_final_uncased_1000.txt \
 --output_dir=gs://deep_speech_bucket/maggie/twitter_results/tweets_bert_no_tweets_pretraining/ \
