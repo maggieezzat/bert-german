2df736ff7b3851325617fe77736224bc476e8c51
maggieezzat <maggieezzat9@gmail.com>
2019-07-16 15:00:46 +0200

    create_data

--- create_data_scripts/create_uncased_data_128.sh
+++ create_data_scripts/create_uncased_data_128.sh
@@ -2,9 +2,9 @@
 
 [ -d /home/maggie/tfrecords ] || mkdir /home/maggie/tfrecords
 
-csv=/home/maggie/csv_files/
-out=/home/maggie/tfrecords/
-vocab=/home/maggie/bert-german/vocab/vocab_bert_final_uncased_1000.txt
+csv=/bert/dewiki_csv_files/
+out=/bert/tfrecords/
+vocab=/bert/bert-german/vocab/bert_vocab_lower_500_wiki.txt
 
 echo $csv
 echo $out
@@ -14,5 +14,5 @@ for file in "$csv"*.csv; do
 	filename=$(basename -- "$file")
 	extension="${filename##*.}"
 	filename="${filename%.*}"
-	python3 /home/maggie/bert-german/create_pretraining_data.py --input_file="$file" --output_file="$out$filename".tfrecord --vocab_file=$vocab --do_lower_case=True --max_seq_length=128 --max_predictions_per_seq=20 --masked_lm_prob=0.15 --random_seed=12345 --dupe_factor=5
+	python3 /bert/bert-german/create_pretraining_data.py --input_file="$file" --output_file="$out$filename".tfrecord --vocab_file=$vocab --do_lower_case=True --max_seq_length=128 --max_predictions_per_seq=20 --masked_lm_prob=0.15 --random_seed=12345 --dupe_factor=5
 done
